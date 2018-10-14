386e6418c63edbfadc43fc64c7fd57fe32441b4e
Ferdinand Prantl <prantlf@gmail.com>
2018-10-14 19:04:51 +0200

    fix: Update sk and ua locales to support the change in the locale format

--- src/locale/sk.js
+++ src/locale/sk.js
@@ -16,28 +16,43 @@ const locale = {
     // 3 plural forms for 1, 2-4, 5-
     pluralRule: 8,
     duration: {
-      s: ['niekoľko sekúnd'],
-      m: ['minúta', '%d minúty', '%d minút'],
-      h: ['hodina', '%d hodiny', '%d hodín'],
-      d: ['deň', '%d dni', '%d dní'],
-      M: ['mesiac', '%d mesiace', '%d mesiacov'],
-      y: ['rok', '%d roky', '%d rokov']
+      s: 'niekoľko sekúnd',
+      m: 'minúta',
+      mm: ['%d minúta', '%d minúty', '%d minút'],
+      h: 'hodina',
+      hh: ['%d hodina', '%d hodiny', '%d hodín'],
+      d: 'deň',
+      dd: ['%d deň', '%d dni', '%d dní'],
+      M: 'mesiac',
+      MM: ['%d mesiac', '%d mesiace', '%d mesiacov'],
+      y: 'rok',
+      yy: ['%d rok', '%d roky', '%d rokov']
     },
     future: {
-      s: ['o niekoľko sekúnd'],
-      m: ['o minútu', 'o %d minúty', 'o %d minút'],
-      h: ['o hodinu', 'o %d hodiny', 'o %d hodín'],
-      d: ['zajtra', 'o %d dni', 'o %d dní'],
-      M: ['o mesiac', 'o %d mesiace', 'o %d mesiacov'],
-      y: ['o rok', 'o %d roky', 'o %d rokov']
+      s: 'o niekoľko sekúnd',
+      m: 'o minútu',
+      mm: ['o %d minútu', 'o %d minúty', 'o %d minút'],
+      h: 'o hodinu',
+      hh: ['o %d hodinu', 'o %d hodiny', 'o %d hodín'],
+      d: 'zajtra',
+      dd: ['o %d deň', 'o %d dni', 'o %d dní'],
+      M: 'o mesiac',
+      MM: ['o %d mesiac', 'o %d mesiace', 'o %d mesiacov'],
+      y: 'o rok',
+      yy: ['o %d rok', 'o %d roky', 'o %d rokov']
     },
     past: {
-      s: ['pred niekoľko sekundami'],
-      m: ['pred minútou', 'pred %d minútami', 'pred %d minútami'],
-      h: ['pred hodinou', 'pred %d hodinami', 'pred %d hodínami'],
-      d: ['včera', 'pred %d dňami', 'pred %d dňami'],
-      M: ['pred mesiacom', 'pred %d mesiacmi', 'pred %d mesiacmi'],
-      y: ['vlani', 'pred %d rokmi', 'pred %d rokmi']
+      s: 'pred niekoľko sekundami',
+      m: 'pred minútou',
+      mm: ['pred %d minútou', 'pred %d minútami', 'pred %d minútami'],
+      h: 'pred hodinou',
+      hh: ['pred %d hodinou', 'pred %d hodinami', 'pred %d hodínami'],
+      d: 'včera',
+      dd: ['pred %d dňom', 'pred %d dňami', 'pred %d dňami'],
+      M: 'pred mesiacom',
+      MM: ['pred %d mesiacom', 'pred %d mesiacmi', 'pred %d mesiacmi'],
+      y: 'vlani',
+      yy: ['pred %d rokom', 'pred %d rokmi', 'pred %d rokmi']
     }
   },
   ordinal: n => `${n}º`
